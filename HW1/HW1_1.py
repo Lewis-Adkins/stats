1e896c89f09904185cd26145d37844057189bd6c
Lewis <lewisadkins10@gmail.com>
2026-09-03 22:24:28 -0400

    revising question 1 and touch ups
     - question 1 part 3 now uses np.cumsum correctly
    - combination formula on 3

--- HW1/HW1_1.py
+++ HW1/HW1_1.py
@@ -2,76 +2,90 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-a = [0, 1]
 
+def q1(n_ex: int = 1000, show = False) -> None:
+    a = [0, 1]
 
-# Experiment: Randomly select an element from the list a
+    # Repeat the experiment n times
 
-result = random.choice(a)
+    results = {}
 
+    for n in range(1,n_ex+1): # size of experiment is equal to the number of experiment
+        # print('-'*5, f'Experiment {n}','-'*5)
+        data = []
+        field = {}
 
-# Repeat the experiment n times
-n_ex = 1000
-
-results = {}
-
-
-for n in range(1,n_ex+1): # size of experiment is equal to the number of experiment
-    print('-'*5, f'Experiment {n}','-'*5)
-    data = []
-    field = {}
-
-    for exp in range(1, n+1):
+        for exp in range(1, n+1):
+            
+            result = random.choice(a)
+            data.append(result)
         
-        result = random.choice(a)
-        data.append(result)
+        field["exp"] = data 
+        field["rf1"] = sum(data) / exp
+        field["rf0"] = 1 - field["rf1"]
+
+
+        results[f'{n} exp'] = field
+
+    rf1s = []
+    rf0s = []
+
+    for ex, val  in results.items():
+        rf1s.append(val['rf1'])
+        rf0s.append(val['rf0'])
+
+    ns = np.arange(len(results))
+
+    plt.title(r'Flipping Coin Outcomes')
+    plt.plot(ns, rf1s, label = "rf1")
+    plt.plot(ns, rf0s, label = "rf0")
+    plt.xlabel("Number of Experiments")
+    plt.ylabel(r"Relative Frequency $R_f$")
+    plt.legend()
+    plt.axhline(.5,  color = 'r', linestyle = '--')
+    if show:
+        plt.show()
+
+
+# # ------------ QUESTION 2 ------------ #
+
+# # There are 2 possible outcomes for 3 trials making the sample space size = 2^3 = 8
+
+# # ------------ QUESTION 3 ------------ #
+
+def q3(n_ex: int = 1000, show = False) -> None:
+    print('=' * 10 ,' Question 3','=' * 10)
+
+    a = [0, 1]
+    # Experiment: Randomly select an element from the list a
+    result = random.choice(a)
+
+    # Repeat the experiment n times
+
+    results = np.random.randint(2, size = n_ex, dtype = int)
     
-    field["exp"] = data
-    field["rf1"] = data.count(1) / n
-    field["rf0"] = data.count(0) / n
+    sizes = np.arange(1,n_ex+1)
 
-    print(f'exp {n} : {field["exp"]}')
-    print(f'rf1 : {field["rf1"]:.3f}')
-    print(f'rf0 : {field["rf0"]:.3f}')
-    results[f'{n} exp'] = field
+    # From Class ==============
+    rf1      = np.cumsum(results) /sizes
+    rf0 = 1 - rf1
 
+    # ==========================
+    print(results) 
 
-rf1s = []
-rf0s = []
-for ex, val  in results.items():
-    rf1s.append(val['rf1'])
-    rf0s.append(val['rf0'])
+    
+    print(rf1)
+    plt.title(r'Cum-Sum No Loop')
+    plt.xlabel("Number of Experiments")
+    plt.ylabel(r"Relative Frequency $R_f$")
+    plt.axhline(.5,  color = 'r', linestyle = '--')
+    plt.plot(sizes, rf1 , label = "rf1")
+    plt.plot(sizes, rf0 , label = "rf0")
+    plt.legend()
+    if show:
+        plt.show()
 
-ns = np.arange(len(results))
-plt.title(r'Flipping Coin Outcomes')
-plt.plot(ns, rf1s)
-plt.plot(ns, rf0s)
-plt.xlabel("Number of Experiments")
-plt.ylabel(r"Relative Frequency $R_f$")
-plt.axhline(.5,  color = 'r', linestyle = '--')
-plt.show()
+q1()
+q3()
 
-
-# ------------ QUESTION 2 ------------ #
-
-# There are 2 possible outcomes for 3 trials making the sample space size = 2^3 = 8
-
-# ------------ QUESTION 3 ------------ #
-
-print('=' * 10 ,' Question 3','=' * 10)
-
-a = [0, 1]
-# Experiment: Randomly select an element from the list a
-result = random.choice(a)
-
-# Repeat the experiment n times
-n = 2
-
-results = np.random.randint(0,1, size = n, dtype = int).tolist()
-
-print(f"n = {n} experiments:")
-print(f"  Results: {results}")
-
-# rf = relative frequency
-print(f"  rf(0) = {results.count(0) / n}")
-print(f"  rf(1) = {results.count(1) / n}")
+### Thoughts: The plots for q1 vs q3 are very different from eachother. is this because q3 resamples an increarely large portion the same dataset while q1 generates a new dataset every single time?
